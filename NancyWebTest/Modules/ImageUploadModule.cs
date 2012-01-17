7ff2ca2b39d63560b56f7db7fddf69b1289aa80a
Richard Astbury <Richard.Astbury@gmail.com>
2012-01-17 12:57:35 +0000

    fixing for VS and win x64

--- NancyWebTest/Modules/ImageUploadModule.cs
+++ NancyWebTest/Modules/ImageUploadModule.cs
@@ -1,64 +1,64 @@
 using System;
-using Nancy;
-using System.IO;
-using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
-using System.Drawing;
+using System.IO;
+using Nancy;
 
 namespace NancyWebTest
 {
-	public class ImageUploadModule : NancyModule
-	{
-		public ImageUploadModule()
+    public class ImageUploadModule : NancyModule
+    {
+        public ImageUploadModule()
         {
-            Get["/"] = parameters => {
+            Get["/"] = parameters =>
+            {
                 return View["index", Request.Url];
             };
-			
-			Post["/"] = parameters => {
-				
-				foreach(var file in Request.Files)
-				{					
-					var imgPath = "/tmp/temp-resized.png";
-				   
-					ResizeImage(file.Value, imgPath, 300, 200, true);
-				}
-				
-				return View["index", Request.Url];
-			};
+
+            Post["/"] = parameters =>
+            {
+
+                foreach (var file in Request.Files)
+                {
+                    var imgPath = "temp-resized.png";
+
+                    ResizeImage(file.Value, imgPath, 300, 200, true);
+                }
+
+                return View["index", Request.Url];
+            };
         }
-		
-		public void ResizeImage(Stream original, string NewFile, int NewWidth, int MaxHeight, bool OnlyResizeIfWider)
-		{
-			System.Drawing.Image FullsizeImage = System.Drawing.Image.FromStream(original);
-		
-			// Prevent using images internal thumbnail
-			FullsizeImage.RotateFlip(System.Drawing.RotateFlipType.Rotate180FlipNone);
-			FullsizeImage.RotateFlip(System.Drawing.RotateFlipType.Rotate180FlipNone);
-		
-			if (OnlyResizeIfWider)
-			{
-				if (FullsizeImage.Width <= NewWidth)
-				{
-					NewWidth = FullsizeImage.Width;
-				}
-			}
-		
-			int NewHeight = FullsizeImage.Height * NewWidth / FullsizeImage.Width;
-			if (NewHeight > MaxHeight)
-			{
-				// Resize with height instead
-				NewWidth = FullsizeImage.Width * MaxHeight / FullsizeImage.Height;
-				NewHeight = MaxHeight;
-			}
-		
-			System.Drawing.Image NewImage = FullsizeImage.GetThumbnailImage(NewWidth, NewHeight, null, IntPtr.Zero);
-		
-			// Clear handle to original file so that we can overwrite it if necessary
-			FullsizeImage.Dispose();
-		
-			// Save resized picture
-			NewImage.Save(NewFile, ImageFormat.Png);
-		}
-	}
+
+        public void ResizeImage(Stream original, string NewFile, int NewWidth, int MaxHeight, bool OnlyResizeIfWider)
+        {
+            System.Drawing.Image FullsizeImage = System.Drawing.Image.FromStream(original);
+
+            // Prevent using images internal thumbnail
+            FullsizeImage.RotateFlip(System.Drawing.RotateFlipType.Rotate180FlipNone);
+            FullsizeImage.RotateFlip(System.Drawing.RotateFlipType.Rotate180FlipNone);
+
+            if (OnlyResizeIfWider)
+            {
+                if (FullsizeImage.Width <= NewWidth)
+                {
+                    NewWidth = FullsizeImage.Width;
+                }
+            }
+
+            int NewHeight = FullsizeImage.Height * NewWidth / FullsizeImage.Width;
+            if (NewHeight > MaxHeight)
+            {
+                // Resize with height instead
+                NewWidth = FullsizeImage.Width * MaxHeight / FullsizeImage.Height;
+                NewHeight = MaxHeight;
+            }
+
+            System.Drawing.Image NewImage = FullsizeImage.GetThumbnailImage(NewWidth, NewHeight, null, IntPtr.Zero);
+
+            // Clear handle to original file so that we can overwrite it if necessary
+            FullsizeImage.Dispose();
+
+            // Save resized picture
+            NewImage.Save(NewFile, ImageFormat.Png);
+        }
+    }
 }
